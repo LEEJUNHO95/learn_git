51ff1f3a88ab0ccb92a01257d4d7ac100009410f
LEEJUNHO <dlwnsgh159@naver.com>
2022-02-08 22:07:52 +0900

    first commit

---
+++ 예시.py
@@ -0,0 +1,19 @@
+import os
+os.system("cls")
+
+treehit = 1
+while treehit < 11:
+    print(f'나무를 {treehit}번 쳤습니다.')
+    treehit += 1
+    if treehit == 11:
+        print("나무가 넘어갔습니다.")
+        break
+
+
+
+
+
+
+
+
+
